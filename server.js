ba62b7f5402e183fe924a6a71024a72533b15c62
Tipoun18 <tipoun1806@gmail.com>
2025-04-19 21:24:05 +0200

    Update server.js

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ io.on("connection", (socket) => {
   });
 });
 
-http.listen(3000, () => {
-  console.log("Serveur lancé sur http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+  console.log(`✅ Serveur lancé sur le port ${PORT}`);
 });
